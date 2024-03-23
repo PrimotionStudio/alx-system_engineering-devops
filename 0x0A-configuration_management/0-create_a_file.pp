804eedb4c6a00bf9d7028d1e2652c2009560805e
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-23 08:09:33 +0100

    configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,16 @@
-file { '/tmp/school':
-  ensure  => 'present',
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+# Using Puppet, create a file in /tmp/school
+# Requirements:
+# File path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
+
+file {'/tmp/school':
+  ensure => 'present',
+  path   => '/tmp/school',
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'www-data',
+  content=> 'I love Puppet',
 }
