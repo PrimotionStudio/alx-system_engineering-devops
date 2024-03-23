6d9ba201c42c47cbf47729360b40633baff6fde9
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-23 08:10:46 +0100

    configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -7,10 +7,10 @@
 # File contains I love Puppet
 
 file {'/tmp/school':
-  ensure => 'present',
-  path   => '/tmp/school',
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
-  content=> 'I love Puppet',
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
