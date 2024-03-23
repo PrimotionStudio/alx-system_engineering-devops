0f7f15977e6736bb8d0734cda7a620b81cc36b7a
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-23 08:59:16 +0100

    configuration_management

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# kill_process.pp
+# Define exec resource to kill the process
+
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+}
