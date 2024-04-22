a14e5b63d3a934029152317ea0ca30ac1e28a6ad
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-22 13:57:55 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,27 +9,26 @@ import json
 from sys import argv
 
 
-emp_id = argv[1]
-# Employee name
-url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
-res = requests.get(url)
-if res.status_code != 200:
-    print("Failed:", res.status_code)
-    exit()
-# Employees Todos
-url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(emp_id)
-_res = requests.get(url)
-if _res.status_code != 200:
-    print("Failed:", _res.status_code)
-    exit()
-total = 0
-comp = 0
-for todo in _res.json():
-    if todo["completed"] is True:
-        comp += 1
-    total += 1
-print("Employee {} is done with tasks({}/{}):".format(dict(res.json())["name"],
-                                                      comp, total))
-for todo in _res.json():
-    if todo["completed"] is True:
-        print("\t {}".format(todo["title"]))
+if __name__ == "__main__":
+    emp_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    res = requests.get(url)
+    if res.status_code != 200:
+        print("Failed:", res.status_code)
+        exit()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(emp_id)
+    _res = requests.get(url)
+    if _res.status_code != 200:
+        print("Failed:", _res.status_code)
+        exit()
+    total = 0
+    comp = 0
+    for todo in _res.json():
+        if todo["completed"] is True:
+            comp += 1
+        total += 1
+    print("Employee {} is done with tasks({}/{}):".format(dict(res.json())["name"],
+                                                          comp, total))
+    for todo in _res.json():
+        if todo["completed"] is True:
+            print("\t {}".format(todo["title"]))
