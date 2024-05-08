6f0ff4b515351c5732412b6f03815e7fae055306
The Primotion Studio <oyedelenewton@gmail.com>
2024-05-08 11:20:38 +0100

    so far with api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+def recurse(subreddit, hot_list=[])
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    a documentation
+    """
+    try:
+        ua = "alx"
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        h = {"User-Agent": ua}
+        res = requests.get(url, headers=h)
+        if res.status_code == 200:
+            data = res.json()
+            count = data["data"]["subscribers"]
+            return hot_list
+        else:
+            raise requests.exceptions.RequestException
+    except (
+        requests.exceptions.RequestException,
+        KeyError,
+        json.decoder.JSONDecodeError,
+    ):
+        return None
