05423583b3dbb0424ecae130acb9590fd39693e2
The Primotion Studio <oyedelenewton@gmail.com>
2024-05-08 13:58:37 +0100

    so far with debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 exec { 'find_and_replace':
   command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/bin', '/usr/bin'],
+  path    => ['/bin', '/usr/bin', '/usr/local/bin'],
   onlyif  => "grep -q phpp /var/www/html/wp-settings.php",
 }
