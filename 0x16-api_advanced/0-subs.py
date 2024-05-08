e3bc56fb34020b2f239e24936697e0df766118c7
The Primotion Studio <oyedelenewton@gmail.com>
2024-05-08 02:21:43 +0100

    so far with api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+curl reddit api
+'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    ua = "alx"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    h = {"User-Agent": ua}
+    res = requests.get(url, headers=h)
+    if res.status_code == 200:
+        data = res.json()
+        count = data["data"]["subscribers"]
+        return (count)
+    else:
+        return (0)
