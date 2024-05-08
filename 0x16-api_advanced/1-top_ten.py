d2446133425870b3bb1893e8988be7a845cad52e
The Primotion Studio <oyedelenewton@gmail.com>
2024-05-08 02:57:05 +0100

    so far with api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+curl reddit api
+"""
+import requests
+from pprint import pprint
+
+
+def top_ten(subreddit):
+    """
+    a documentation
+    """
+    ua = "alx"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    h = {"User-Agent": ua}
+    res = requests.get(url, headers=h)
+    if res.status_code == 200:
+        data = res.json()
+        j = 0
+        for i in data["data"]["children"]:
+            if j < 10:
+                print(i["data"]["title"])
+            else:
+                break
+            j += 1
+    else:
+        return (None)
