bad3231a118d3d1c559ffa86980d1eef01a12bf9
Primotion Studio <oyedelenewton@gmail.com>
2024-05-17 17:06:57 +0100

    plenty

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,5 @@
 # Define a class to manage user configurations
 exec { 'change-os-configuration-for-holberton-user':
-  command => [
-    '/bin/bash -c "echo \'holberton soft nofile 4096\' >> /etc/security/limits.conf"',
-    '/bin/bash -c "echo \'holberton hard nofile 8192\' >> /etc/security/limits.conf"',
-    '/bin/bash -c "echo \'session required pam_limits.so\' >> /etc/pam.d/common-session"',
-    '/bin/bash -c "echo \'session required pam_limits.so\' >> /etc/pam.d/common-session-noninteractive"'
-  ],
-  unless  => [
-    '/bin/bash -c "grep -q \'holberton soft nofile 4096\' /etc/security/limits.conf"',
-    '/bin/bash -c "grep -q \'holberton hard nofile 8192\' /etc/security/limits.conf"',
-    '/bin/bash -c "grep -q \'session required pam_limits.so\' /etc/pam.d/common-session"',
-    '/bin/bash -c "grep -q \'session required pam_limits.so\' /etc/pam.d/common-session-noninteractive"'
-  ],
+  command => 'ex -s -c "/holberton hard/s/5/50000/g|x" /etc/security/limits.conf',
+  path    => '/usr/bin/:/bin/'
 }
