a872c5283f813ebd5bb2f3a87f306594b5d815be
Primotion Studio <oyedelenewton@gmail.com>
2024-05-17 15:17:37 +0100

    same

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,14 @@
-# Define a class to manage user configurations
 exec { 'change-os-configuration-for-holberton-user':
-  command => '/bin/bash -c "echo \'holberton soft nofile 4096\' >> /etc/security/limits.conf && echo \'holberton hard nofile 8192\' >> /etc/security/limits.conf && echo \'session required pam_limits.so\' >> /etc/pam.d/common-session && echo \'session required pam_limits.so\' >> /etc/pam.d/common-session-noninteractive"',
-  unless  => '/bin/bash -c "grep -q \'holberton soft nofile 4096\' /etc/security/limits.conf && grep -q \'holberton hard nofile 8192\' /etc/security/limits.conf && grep -q \'session required pam_limits.so\' /etc/pam.d/common-session && grep -q \'session required pam_limits.so\' /etc/pam.d/common-session-noninteractive"',
+  command => [
+    '/bin/bash -c "echo \'holberton soft nofile 4096\' >> /etc/security/limits.conf"',
+    '/bin/bash -c "echo \'holberton hard nofile 8192\' >> /etc/security/limits.conf"',
+    '/bin/bash -c "echo \'session required pam_limits.so\' >> /etc/pam.d/common-session"',
+    '/bin/bash -c "echo \'session required pam_limits.so\' >> /etc/pam.d/common-session-noninteractive"'
+  ],
+  unless  => [
+    '/bin/bash -c "grep -q \'holberton soft nofile 4096\' /etc/security/limits.conf"',
+    '/bin/bash -c "grep -q \'holberton hard nofile 8192\' /etc/security/limits.conf"',
+    '/bin/bash -c "grep -q \'session required pam_limits.so\' /etc/pam.d/common-session"',
+    '/bin/bash -c "grep -q \'session required pam_limits.so\' /etc/pam.d/common-session-noninteractive"'
+  ],
 }
