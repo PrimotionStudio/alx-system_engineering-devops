4e48cc0c951d87adcff81e4aac3685b5e2fda704
Primotion Studio <oyedelenewton@gmail.com>
2024-05-17 17:08:20 +0100

    bass

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Define a class to manage user configurations
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'ex -s -c "/holberton hard/s/5/50000/g|x" /etc/security/limits.conf',
+  command => 'ex -s -c "/holberton hard/s/5/99999999/g|x" /etc/security/limits.conf',
   path    => '/usr/bin/:/bin/'
 }
