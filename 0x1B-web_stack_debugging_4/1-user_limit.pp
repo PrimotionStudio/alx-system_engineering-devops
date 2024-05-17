c0e7835fd9222d4b32aa3e61dda01482d9b0dd79
Primotion Studio <oyedelenewton@gmail.com>
2024-05-17 14:59:57 +0100

    fixed 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,16 +1,5 @@
 # Define a class to manage user configurations
-class user_config {
-
-  # Define a resource to set ulimit for the holberton user
-  exec { 'increase-file-limit':
-    command => 'ulimit -n 65535', # Set the maximum number of open file descriptors
-    user    => 'holberton',        # Specify the user for whom the limit will be set
-    path    => '/bin:/usr/bin',     # Set the PATH to ensure the ulimit command is found
-    unless  => 'test $(ulimit -n) -eq 65535', # Check if the limit is already set
-    notify  => Service['ssh'],      # Restart SSH service if the limit is changed
-  }
+exec { 'change-os-configuration-for-holberton-user':
+  command => '/bin/bash -c "echo \'holberton soft nofile 4096\' >> /etc/security/limits.conf && echo \'holberton hard nofile 8192\' >> /etc/security/limits.conf && echo \'session required pam_limits.so\' >> /etc/pam.d/common-session && echo \'session required pam_limits.so\' >> /etc/pam.d/common-session-noninteractive"',
+  unless  => '/bin/bash -c "grep -q \'holberton soft nofile 4096\' /etc/security/limits.conf && grep -q \'holberton hard nofile 8192\' /etc/security/limits.conf && grep -q \'session required pam_limits.so\' /etc/pam.d/common-session && grep -q \'session required pam_limits.so\' /etc/pam.d/common-session-noninteractive"',
 }
-
-# Include the class to apply the configuration
-include user_config
-
