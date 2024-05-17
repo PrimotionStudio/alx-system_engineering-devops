033a97e26568b4447a779f71f0e799dc0243f3dd
Primotion Studio <oyedelenewton@gmail.com>
2024-05-17 17:16:23 +0100

    yea

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Define a class for Nginx configuration
 exec { 'change-os-configuration-for-holberton-user':
-  command => 'ex -s -c "/ULIMIT/s/15/9999/g|x" /etc/default/nginx',
+  command => 'ex -s -c "/ULIMIT/s/15/4096/g|x" /etc/default/nginx',
   path    => '/usr/bin/:/bin/'
 }
